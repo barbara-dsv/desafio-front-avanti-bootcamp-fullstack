304418b07e4197aa9a211e4713ed908a39b9a9ca
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-03-03 15:04:28 -0300

    :sparkles:	feat: estrutura inicial do projeto adicionada

---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+
+export const routes = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+
+        </Route>
+    )
+)
