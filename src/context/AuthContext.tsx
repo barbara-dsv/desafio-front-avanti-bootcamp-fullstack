159bda5d1efadb579e4bffb4c0da816bcd6011a1
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-03-03 15:06:09 -0300

    :sparkles:	feat: adicionada configuração de estado de autenticação

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useState, useEffect } from "react";
+import type { ReactNode } from "react";
+
+interface AuthContextProps {
+    isAuthenticated: boolean;
+    login: (token: string) => void;
+    logout: () => void;
+    loading: boolean;
+}
+
+
+export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            setIsAuthenticated(true);
+        }
+        setLoading(false);
+    }, []);
+
+    const login = (token: string) => {
+        localStorage.setItem("token", token);
+        setIsAuthenticated(true);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setIsAuthenticated(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, login, logout, loading }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+};
