06529a219d4ad567c4122e86c985ddf6d3611c61
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-03-06 11:21:11 -0300

    :bug:	fix: corrigindo rota de singup

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,16 +2,15 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 
 import { LandingPage } from "./pages/LadingPage";
 import { SignupPage } from "./pages/SingUp";
-import {LoginPage} from "./pages/LoginPage";
+import { LoginPage } from "./pages/LoginPage";
 //import {DashboardPage} from "./pages/dashboardPage";
 
 export const routes = createBrowserRouter(
     createRoutesFromElements(
-       <Route>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      
-    </Route>
+        <Route>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/singup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+        </Route>
     )
 )
