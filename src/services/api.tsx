101a4d0142c824eb2d40268029d54484df78ea65
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-03-06 12:51:02 -0300

    :recycle:	refactor: altera url local para o deploy do backend

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 
